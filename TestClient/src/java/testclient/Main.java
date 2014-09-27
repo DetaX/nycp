3ac859e4304777764e82b179777b414f03c854c1
DetaX <DetaX@DetaX-PC.home>
2014-09-27 16:34:35 +0200

    Adding test scenario for prisoner management

--- TestClient/src/java/testclient/Main.java
+++ TestClient/src/java/testclient/Main.java
@@ -1,16 +1,23 @@
 package testclient;
-
+import nycpEJB.Prisoner_managementLocal;
 /**
  *
  * @author DetaX
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            javax.naming.Context jndi_context = new javax.naming.InitialContext();
+            Prisoner_managementLocal pm = (Prisoner_managementLocal) jndi_context.lookup("ejb/Prisoner_management+");
+            pm.insert_prisoner("Rémi", "Douteaud", "16-12-90", "Briey");
+            if (jndi_context != null) {
+                jndi_context.close();
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
     }
     
 }
