a33a3a1d3bfc32ac3bf9e15186279abea02fa989
DetaX <DetaX@DetaX-PC.home>
2014-09-28 16:51:46 +0200

    testing criminalcase, prisonercriminalcase, incarceration session beans

--- TestClient/src/java/testclient/Main.java
+++ TestClient/src/java/testclient/Main.java
@@ -1,5 +1,8 @@
 package testclient;
-import nycpEJB.Prisoner_managementLocal;
+import session.PrisonerLocal;
+import session.CriminalCaseLocal;
+import session.PrisonerCriminalCaseLocal;
+import session.IncarcerationLocal;
 import java.util.Date;
 import javax.ejb.EJB;
 import java.util.Calendar;
@@ -12,14 +15,20 @@ public class Main {
         
         try {
             javax.naming.Context jndi_context = new javax.naming.InitialContext();
-            Prisoner_managementLocal pm = (Prisoner_managementLocal) jndi_context.lookup("ejb/Prisoner_management");
+            PrisonerLocal pm = (PrisonerLocal) jndi_context.lookup("ejb/Prisoner");
+            CriminalCaseLocal ccm = (CriminalCaseLocal) jndi_context.lookup("ejb/CriminalCase");
+            PrisonerCriminalCaseLocal pccm = (PrisonerCriminalCaseLocal) jndi_context.lookup("ejb/PrisonerCriminalCase");
+            IncarcerationLocal im = (IncarcerationLocal) jndi_context.lookup("ejb/Incarceration");
             Date birthDate;
             Calendar cal = Calendar.getInstance();
             cal.set(Calendar.MONTH, 9);
             cal.set(Calendar.DATE, 24);
             cal.set(Calendar.YEAR, 2013);
             birthDate = cal.getTime();
-            pm.insert_prisoner("Rémi", "Douteaud", birthDate, "Briey");
+            //pm.insert_prisoner("Rémi", "Douteaud", birthDate, "Briey");
+            //ccm.insert_criminal_case("UPPA", birthDate);
+            //pccm.insert_prisoner_criminal_case("1", "0", "UPPA");
+            im.insert_incarceration("1", "0", "UPPA", birthDate, "01");
             if (jndi_context != null) {
                 jndi_context.close();
             }
