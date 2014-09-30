cada52e477aa1647f43ce7044bdd113e4bec16ff
DetaX <DetaX@DetaX-PC.home>
2014-09-30 13:32:38 +0200

    Refactoring Incarceration use case

--- nycpEJB/src/java/session/IncarcerationBean.java
+++ nycpEJB/src/java/session/IncarcerationBean.java
@@ -2,6 +2,8 @@ package session;
 import entity.Incarceration;
 import entity.Motive;
 import entity.PrisonerCriminalCase;
+import entity.CriminalCase;
+import entity.Prisoner;
 import java.util.Date;
 import javax.ejb.Stateless;
 
@@ -25,5 +27,41 @@ public class IncarcerationBean implements IncarcerationLocal {
          _entity_manager.persist(i);
     }
 
+    @Override
+    public void insert_prisoner_criminal_case(String prisonFileNumber, String criminalCaseNumber, String jurisdictionName) {
+        PrisonerCriminalCase pcc = new PrisonerCriminalCase(prisonFileNumber, criminalCaseNumber, jurisdictionName);
+         _entity_manager.persist(pcc);
+    }
+    
+    @Override
+    public String insert_criminal_case(String jurisdictionName, Date date) {
+        String _query = (String)_entity_manager.createQuery("select max(c.criminalCasePK.criminalCaseNumber) from CriminalCase c").getSingleResult();
+        Integer case_number = (_query == null) ? 0 : Integer.valueOf(_query) + 1;
+        CriminalCase cc = new CriminalCase(case_number.toString(), jurisdictionName);
+        cc.setDateOfCriminalCase(date);
+        _entity_manager.persist(cc);
+        return case_number.toString();
+    }
+    
+    @Override
+    public String insert_prisoner(String name, String surname, Date birthDate, String birthPlace) {
+        String _query = (String)_entity_manager.createQuery("select max(p.prisonFileNumber) from Prisoner p").getSingleResult();
+        Integer file_number = (_query == null) ? 0 : Integer.valueOf(_query) + 1;
+        Prisoner prisoner = new Prisoner(file_number.toString());
+        prisoner.setGivenName(name);
+        prisoner.setSurname(surname);
+        prisoner.setDateOfBirth(birthDate);
+        prisoner.setPlaceOfBirth(birthPlace);
+        _entity_manager.persist(prisoner);
+        return file_number.toString();
+    }
+
+    @Override
+    public void incarcerate(String name, String surname, Date birthDate, String birthPlace, String jurisdictionName, Date date, Date incarcerationDate, String motive) {
+       String file_number = insert_prisoner(name,surname,birthDate,birthPlace);
+       String case_number = insert_criminal_case(jurisdictionName, date);
+       insert_prisoner_criminal_case(file_number,case_number,jurisdictionName);
+       insert_incarceration(file_number,case_number,jurisdictionName,incarcerationDate,motive);
+    }
 
 }
--- nycpEJB/src/java/session/IncarcerationLocal.java
+++ nycpEJB/src/java/session/IncarcerationLocal.java
@@ -9,4 +9,8 @@ import javax.ejb.Remote;
 @Remote
 public interface IncarcerationLocal {
     void insert_incarceration(final String prisonFileNumber, final String criminalCaseNumber, final String jurisdictionName, final Date incarcerationDate, final String motive);
+    void insert_prisoner_criminal_case(final String prisonFileNumber, final String criminalCaseNumber, final String jurisdictionName);
+    String insert_criminal_case(final String jurisdictionName, final Date date);
+    String insert_prisoner(final String name, final String surname, final Date birthDate, final String birthPlace);
+    void incarcerate(final String name, final String surname, final Date birthDate, final String birthPlace, final String jurisdictionName, final Date date, final Date incarcerationDate, final String motive);
 }
