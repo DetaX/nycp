02e647f7e900a19d5f740c6e9e3a04c5a9d17baf
DetaX <DetaX@DetaX-PC.home>
2014-10-01 19:40:27 +0200

    testing judicial decisions

--- TestClient/src/java/testclient/Main.java
+++ TestClient/src/java/testclient/Main.java
@@ -2,6 +2,7 @@ package testclient;
 import session.IncarcerationLocal;
 import java.util.Date;
 import java.util.Calendar;
+import session.JudicialDecisionLocal;
 /**
  *
  * @author DetaX
@@ -12,14 +13,15 @@ public class Main {
         try {
             javax.naming.Context jndi_context = new javax.naming.InitialContext();
             IncarcerationLocal im = (IncarcerationLocal) jndi_context.lookup("ejb/Incarceration");
+            JudicialDecisionLocal jdm = (JudicialDecisionLocal) jndi_context.lookup("ejb/JudicialDecision");
             Date birthDate;
             Calendar cal = Calendar.getInstance();
             cal.set(Calendar.MONTH, 8);
             cal.set(Calendar.DATE, 30);
             cal.set(Calendar.YEAR, 2014);
             birthDate = cal.getTime();
-            im.incarcerate("Pierre", "Caretero", birthDate, "Talence", "UPPA", birthDate, birthDate, "01");
-            
+            //im.incarcerate("Pierre", "Caretero", birthDate, "Talence", "UPPA", birthDate, birthDate, "01");
+            jdm.insert_final_discharge("0", birthDate, birthDate);
             if (jndi_context != null) {
                 jndi_context.close();
             }
