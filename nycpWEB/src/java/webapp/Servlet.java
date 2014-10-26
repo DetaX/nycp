f536a9b19446da08a13e8a644669bd9347489ebe
DetaX <DetaX@DetaX-PC.home>
2014-10-26 11:23:09 +0100

    adding under_remand

--- nycpWEB/src/java/webapp/Servlet.java
+++ nycpWEB/src/java/webapp/Servlet.java
@@ -5,10 +5,12 @@
  */
 package webapp;
 
+import entity.Prisoner;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
@@ -23,6 +25,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import session.IncarcerationLocal;
 import session.JudicialDecisionLocal;
+import session.UnderRemandLocal;
 
 /**
  *
@@ -42,6 +45,8 @@ public class Servlet extends HttpServlet {
     private IncarcerationLocal incarceration;
     @EJB
     private JudicialDecisionLocal judicial_decision;
+    @EJB
+    private UnderRemandLocal under_remand;
     @javax.persistence.PersistenceContext(name = "nycpEJBPU")
     private javax.persistence.EntityManager _entity_manager;
     
@@ -51,7 +56,31 @@ public class Servlet extends HttpServlet {
         request.setAttribute("prisoners",results);
     }
             
-            
+    private void setPrisoners(HttpServletRequest request, List<Prisoner> prisoners) {
+        List<String[]> results = new ArrayList<>();
+        for (Prisoner p : prisoners) {
+            String[] info = new String[5];
+            info[0] = p.getGivenName();
+            info[1] = p.getSurname();
+            info[2] = p.getPlaceOfBirth();
+            info[3] = p.getPrisonFileNumber();
+            info[4] = p.getDateOfBirth().toString();
+            results.add(info);
+        }
+        request.setAttribute("prisoners",results);
+    }       
+    
+    private void setJurisdiction(HttpServletRequest request){
+        TypedQuery<String> query = _entity_manager.createQuery("SELECT c.criminalCasePK.jurisdictionName from CriminalCase c", String.class);
+        List<String> results = query.getResultList();
+        request.setAttribute("jurisdiction", results);
+    }
+    
+    private void setMotive(HttpServletRequest request){
+        TypedQuery<String[]> query = _entity_manager.createQuery("SELECT m.motiveNumber, m.motiveLabel from Motive m", String[].class);
+        List<String[]> results = query.getResultList();
+        request.setAttribute("motive", results);
+    }
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -69,16 +98,33 @@ public class Servlet extends HttpServlet {
         RequestDispatcher dispatcher;
         switch(request.getServletPath()) {
             case "/incarcerate":
-                String test= "Teeeeest";
-                request.setAttribute("test",test);
+                setJurisdiction(request);
+                setMotive(request);
                 dispatcher = request.getRequestDispatcher("WEB-INF/incarcerate.jsp");
                 dispatcher.forward(request,response);
                 break;
             case "/incarcerate/new":
-                // TODO
+                String name = request.getParameter("name");
+                String surname = request.getParameter("surname");
+                String bd = request.getParameter("birthDate");
+                Date birthDate = new SimpleDateFormat("yyyy-MM-dd").parse(bd);
+                String birthPlace = request.getParameter("birthPlace");
+                String jurisdiction = request.getParameter("jurisdiction");
+                String id = request.getParameter("incarcerationDate");
+                Date incarcerationDate = new SimpleDateFormat("yyyy-MM-dd").parse(id);
+                String d = request.getParameter("date");
+                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(d);
+                String motive = request.getParameter("motive");
+                incarceration.incarcerate(name, surname, birthDate, birthPlace, jurisdiction, date, incarcerationDate, motive);
+                out.println("<a href='../index.html'>Home</a><br/>");
+                out.println("Incarcerated prisoner : " + name + " " + "surname" + ", born on " + birthDate + " in " + birthPlace + "(" + jurisdiction + "). Criminal case pronounced on " + date);
+                out.println("<br/>Incarcerated on " + incarcerationDate + " for motive : " + motive);
                 break;
             case "/under_remand":
-                // TODO
+                List<Prisoner> prisoners = under_remand.get_under_remand();
+                setPrisoners(request, prisoners);
+                dispatcher = request.getRequestDispatcher("WEB-INF/under_remand.jsp");
+                dispatcher.forward(request,response);
                 break;
             case "/conviction":
                 setPrisoners(request);
@@ -89,8 +135,9 @@ public class Servlet extends HttpServlet {
                 Integer duration = Integer.valueOf(request.getParameter("duration"));
                 String strDate = request.getParameter("date");
                 String prisoner = request.getParameter("prisoner");
-                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+                date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
                 judicial_decision.insert_conviction(prisoner, date, duration);
+                out.println("<a href='../index.html'>Home</a><br/>");
                 out.println("Convicted prisoner " + prisoner + " to " + duration + " days");
                 break;
             case "/final_discharge":
@@ -105,6 +152,7 @@ public class Servlet extends HttpServlet {
                 strDate = request.getParameter("dischargeDate");
                 Date dischargeDate = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
                 judicial_decision.insert_final_discharge(prisoner, decisionDate, dischargeDate);
+                out.println("<a href='../index.html'>Home</a><br/>");
                 out.println("Prisoner "+ prisoner + " gets discharged on " + dischargeDate);
                 break;
             case "/shortened_sentence":
@@ -118,6 +166,7 @@ public class Servlet extends HttpServlet {
                 prisoner = request.getParameter("prisoner");
                 date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
                 judicial_decision.insert_shortened_sentence(prisoner, date, duration);
+                out.println("<a href='../index.html'>Home</a><br/>");
                 out.println("Shortened sentence by " + duration + " days of prisoner " + prisoner);
                 break;
         }
--- nycpWEB/web/WEB-INF/under_remand.jsp
+++ nycpWEB/web/WEB-INF/under_remand.jsp
@@ -4,14 +4,34 @@
     Author     : DetaX
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Prisoners under remand</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Prisoners under remand</h1>
+        <a href="index.html">Home</a>
+        <table>
+    <tr>
+    <th>prison file number</th>
+    <th>given name</th>
+    <th>surname</th>
+    <th>date of birth</th>
+    <th>place of birth</th>
+    </tr>
+            <c:forEach var="prisoner" items="${prisoners}">
+                <tr>
+                    <td>${prisoner[3]}</td>
+                    <td>${prisoner[1]}</td>
+                    <td>${prisoner[1]}</td>
+                    <td>${prisoner[4]}</td>
+                    <td>${prisoner[2]}</td>
+                </tr>
+             </c:forEach>
+        </table>
     </body>
 </html>
